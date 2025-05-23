0d61ac4717284049504f71c4f7077c1425317a8d
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-05-24 00:01:06 +0200

    add my experience and education background in database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,58 @@
-import express from 'express'
+import express,{Request,Response} from 'express'
 import env from './config/env'
 import connectdb from './config/dbConnect'
+import education from './models/education.models'
+import experience from './models/experience.modules'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET','POST','PUT','DELETE','PATCH']
+}))
 
+app.post('/education', async(req: Request, res:Response)=>{
+    try {
+       const {startYear,endYear,program,school,degree} = req.body
+       await education.create({
+        startYear,
+        endYear,
+        program,
+        school,
+        degree,
+       })
+       res.status(201).json({message:'Education uploaded successfully!'})
+    } catch (error: any) {
+        res.status(500).json({message:"failed to create education",error:error.essage})
+    }
+})
+
+app.get('/education', async(req: Request, res:Response)=>{
+    try {
+        const educations =  await education.find()
+        res.status(201).json(educations)
+    } catch (error) {
+        res.status(500).json({message:'Failed to fetch education data'})
+    }
+})
+
+app.post('/experience', async (req:Request, res:Response)=>{
+    try {
+       const {company,role,employment,startDate,endDate,description} = req.body
+       await experience.create({
+        company,
+        role,
+        employment,
+        startDate,
+        endDate,
+        description
+       })
+       res.status(201).json({message:'experience uploaded successfully!'})
+    } catch (error) {
+        res.status(500).json({message:'failed to create experience'})
+    }
+})
 const PORT = env.port
 app.listen(PORT, async()=>{
     try {
