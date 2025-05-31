56667cb47953cc6c70834f04b029efb1d72a5cea
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-05-31 12:35:54 +0200

    change the link

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import skillRoutes from './Routers/skill.router'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173','https://mucyo-papy-blaise.netlify.app/'],
+    origin: ['http://localhost:5173','https://mucyo-papy-blaise.netlify.app'],
     methods: ['GET','POST','PUT','DELETE','PATCH']
 }))
 
