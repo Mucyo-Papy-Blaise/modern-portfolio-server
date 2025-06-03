e8dd3789a64d6dda7cdb41ed06ddcd065f76ee7f
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-06-03 10:37:08 +0200

    new changes on links

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ['http://localhost:5173', 'https://mucyo-papy-blaise.netlify.app'],
-    methods: ['GET','POST','PUT','DELETE','PATCH']
+    methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS']
 }))
 
 app.use('/education', educationRoutes)
