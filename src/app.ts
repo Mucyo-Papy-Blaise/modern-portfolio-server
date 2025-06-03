45757042f44f31a8bb4493704f13d37fa7144417
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-06-03 10:44:38 +0200

    add headrs to cors config

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ['http://localhost:5173', 'https://mucyo-papy-blaise.netlify.app'],
-    methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS']
+    methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }))
 
 app.use('/education', educationRoutes)
